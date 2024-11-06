1ac02fa6718488fb26eb63a4a175a38f09ed9a7a
cleirewi <wim.cleiren@district09.gent>
2024-11-06 09:43:38 +0100

    chore: init

---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace net_service_d09.console.template
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
